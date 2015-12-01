2e0a4a2edb53f4da8ccbb0c62719c297726d4e83
do0fusz <do0fusz@hotmail.com>
2015-12-01 11:51:06 +0100

    Made Show page look better

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130163620) do
+ActiveRecord::Schema.define(version: 20151201104557) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151130163620) do
     t.boolean  "availability", default: true
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "author"
   end
 
   create_table "users", force: :cascade do |t|
